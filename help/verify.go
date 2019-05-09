08987a540114faaa22d0bca836882f8d07a6fa63
liangyu <liangyu@22.cn>
2019-05-09 18:00:14 +0800

    格式校验工具

---
+++ help/verify.go
@@ -0,0 +1,19 @@
+package help
+
+import "regexp"
+
+//该文件是验证工具库
+
+//邮箱格式验证
+func VerifyEmailFormat(email string) bool {
+	pattern := `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
+
+//手机号格式验证
+func VerifyMobileFormat(mobileNum string) bool {
+	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+	reg := regexp.MustCompile(regular)
+	return reg.MatchString(mobileNum)
+}
