ff901f90762a6a327124ba586ef3bf9e03d53873
liangyu <liangyu@22.cn>
2019-05-10 09:16:41 +0800

    数据库

--- frame_tool/frame.go
+++ frame_tool/frame.go
@@ -20,11 +20,13 @@ func (this *LyFrameTool) initMysql() {
 	if err != nil {
 		log.Fatalln("数据库配置文件加载失败", err)
 	}
+	ip := viper.GetString("ip")
+	port := viper.GetString("port")
 	user := viper.GetString("user")
 	passwd := viper.GetString("passwd")
 	db := viper.GetString("db")
-	log.Println(fmt.Sprintf("连接数据库的信息;user:%s,passwd:%s,db:%s", user, passwd, db))
-	database.CreateMysqlConnection(user, passwd, db)
+	log.Println( fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", user, passwd, ip, port, db))
+	database.CreateMysqlConnection(user, passwd, ip, port, db)
 }
 
 //从配置文件中获取端口
