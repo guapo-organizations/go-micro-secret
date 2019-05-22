5e5b007a8232e5c0dc8f252bb6a1ab911f7d6805
liangyu <liangyu@22.cn>
2019-05-22 11:10:55 +0800

    换成共有方法

--- consul/find_service.go
+++ consul/find_service.go
@@ -7,13 +7,11 @@ import (
 
 //consul过滤的语法参考这里
 //https://www.consul.io/api/agent/service.html
-func FindService(config *api.Config, name string, address string, port string) (*api.AgentService, error) {
+func FindService(config *api.Config, service_id string) (*api.AgentService, error) {
 	client, err := api.NewClient(config)
 	if err != nil {
 		return nil, fmt.Errorf("consul 客户端FindService错误 :", err)
 	}
-
-	service_id := createAgentServiceUniqueID(name, address, port)
 
 	//找到服务ID 等于 server_id的服务
 	services, err := client.Agent().ServicesWithFilter(service_id + " Equal ID")
@@ -24,7 +22,7 @@ func FindService(config *api.Config, name string, address string, port string) (
 	service, ok := services[service_id]
 
 	if !ok {
-		return nil, fmt.Errorf("找不到名字为:%s,地址:%s,端口:%s的服务", name, address, port)
+		return nil, fmt.Errorf("找不到id为:%s的服务", service_id)
 	}
 
 	return service, nil
--- consul/service_id.go
+++ consul/service_id.go
@@ -6,7 +6,7 @@ import (
 )
 
 //创建服务的唯一id
-func createAgentServiceUniqueID(name string, address string, port string) string {
+func CreateAgentServiceUniqueID(name string, address string, port string) string {
 	ID := fmt.Sprintf("service-name:%s,service-address:%s,service-port:%s", name, address, port)
 	unique_id := md5.Sum([]byte(ID))
 	return fmt.Sprintf("%x", unique_id)
