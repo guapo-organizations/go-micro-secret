28eb11d0f4e2acb85b1a260a6276703aec7b7474
liangyu <liangyu@22.cn>
2019-05-24 20:48:58 +0800

    添加连接服务发现

--- consul/config.go
+++ consul/config.go
@@ -5,9 +5,18 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
+var config *api.Config
+
 //创建连接服务发现的config配置对象
 func CreateConfig(ip string, port string) (config *api.Config) {
 	config = api.DefaultConfig()
 	config.Address = fmt.Sprintf("%s:%s", ip, port)
 	return config
+}
+
+func GetConfig() (*api.Config, error) {
+	if config == nil {
+		return nil, fmt.Errorf("consul链接信息没有加载到缓存中")
+	}
+	return config, nil
 }
---
+++ grpc/connect.go
@@ -0,0 +1,33 @@
+package grpc
+
+import (
+	"fmt"
+	"github.com/guapo-organizations/go-micro-secret/consul"
+	"github.com/guapo-organizations/go-micro-secret/tls"
+	"github.com/hashicorp/consul/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+//链接服务
+func GetGrpcConnet(servier_name string, tag string, config *api.Config, ) (*grpc.ClientConn, error) {
+	agent_service, err := consul.FindService(config, servier_name, tag)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//tls 和 ssl加密
+	creds, err := credentials.NewClientTLSFromFile(tls.Path("ca.pem"), "zldz.com")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", agent_service.ID, agent_service.Port), grpc.WithTransportCredentials(creds))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
