2ddef86f1351f938649b337b4ad93f6acceefab6
liangyu <liangyu@22.cn>
2019-05-28 14:08:49 +0800

    golang断言应用

--- jwt/token_test.go
+++ jwt/token_test.go
@@ -17,7 +17,7 @@ func TestJwtTokenEncode(t *testing.T) {
 	toek_map := jwt.MapClaims{}
 	toek_map["name"] = "liangyu"
 	toek_map["my_data"] = my_data
-	token, err := JwtTokenEncode(toek_map, 1)
+	token, err := JwtTokenEncode(toek_map, 60)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,11 +27,23 @@ func TestJwtTokenEncode(t *testing.T) {
 //jwt解码测试
 func TestJwtTokenDecode(t *testing.T) {
 
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTg5MzkxNDEsIm15X2RhdGEiOlsi5qKB5a6HIiwi6IOh55uK6ZOtIiwi5YiY5Y2w5Y2IIiwi5bCP5piOIiwi6Zi_6IqxIl0sIm5hbWUiOiJsaWFuZ3l1In0.IsIMZmIvWQ2uUsctVWRZSsH36m0pSQhTqnxXhjGQMIc"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTkwMjcyMDMsIm15X2RhdGEiOlsi5qKB5a6HIiwi6IOh55uK6ZOtIiwi5YiY5Y2w5Y2IIiwi5bCP5piOIiwi6Zi_6IqxIl0sIm5hbWUiOiJsaWFuZ3l1In0.pMUtC6FeY6wMYSnLPdHPykEUaTKLiAJMd4rG8WQi2Qg"
 
 	jwt_map, err := JwtTokenDecode(token)
 	if err != nil {
 		log.Fatalf("错误%v", err)
 	}
-	log.Printf("结果%v", jwt_map)
+
+	log.Println(jwt_map["my_data"])
+	//这里会输出变量的类型
+	log.Printf("%T",jwt_map["my_data"])
+	//动态打印一下类型
+	switch my_data_type := jwt_map["my_data"].(type) {
+	case []interface{}:
+		log.Println("切片")
+	default:
+		log.Println("什么类型？")
+		log.Println(my_data_type)
+	}
+
 }
